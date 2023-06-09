d32397a5936c020972d0321baa60decfb160bc4a
Debarshi Das <debarshi121@gmail.com>
2023-06-09 22:12:45 +0530

    Session-09

--- Week-3/Session-09/JavaScript number guessing game/script.js
+++ Week-3/Session-09/JavaScript number guessing game/script.js
@@ -1,1 +1,34 @@
-// Your solution goes here 
+// Your solution goes here
+const playGuessingGame = (numToGuess, totalGuesses = 10) => {
+	let guessCount = 0;
+	let guess = prompt(`Enter a number between 1 and 100.`);
+
+	if (guess === null) {
+		return 0;
+	} else {
+		if (guess === numToGuess) {
+			return 1;
+		} else {
+			guessCount++;
+			while (guessCount < totalGuesses) {
+				if (isNaN(parseInt(guess))) {
+					guess = prompt(`Please enter a number.`);
+				}
+				if (guess < numToGuess) {
+					guess = prompt(`${guess} is too small. Guess a larger number.`);
+					guessCount++;
+				} else if (guess > numToGuess) {
+					guess = prompt(`${guess} is too large. Guess a smaller number.`);
+					guessCount++;
+				}
+				if (guess == numToGuess) {
+					return guessCount;
+				}
+				if (guess === null) {
+					return 0;
+				}
+			}
+			return 0;
+		}
+	}
+};
--- Week-3/Session-09/JavaScript password strength/script.js
+++ Week-3/Session-09/JavaScript password strength/script.js
@@ -1,1 +1,12 @@
-// Your solution goes here 
+// Your solution goes here
+const isStrongPassword = (password) => {
+	password = password.toString();
+	if (password.length >= 8) {
+		if (password.toLowerCase() !== "password") {
+			if (/[A-Z]/.test(password)) {
+				return true;
+			}
+		}
+	}
+	return false;
+};
