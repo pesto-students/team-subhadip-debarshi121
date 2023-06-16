c859ba4e42c29449632418f5b7a8437d3ed82468
Debarshi Das <debarshi121@gmail.com>
2023-06-16 22:48:32 +0530

    Week 3

--- Week-3/Session-09/JavaScript number guessing game/script.js
+++ Week-3/Session-09/JavaScript number guessing game/script.js
@@ -3,33 +3,24 @@ const playGuessingGame = (numToGuess, totalGuesses = 10) => {
 	let guessCount = 0;
 	let guess = Number(prompt(`Enter a number between 1 and 100.`));
 
-	if (guess === null) {
-		return 0;
-	} else {
+	while (guessCount <= totalGuesses) {
+		if (guess === null) return 0;
+		if (guess === 0) return 0;
 		if (guess === numToGuess) {
-			return 1;
-		} else {
-			guessCount++;
-			while (guessCount < totalGuesses) {
-				switch (true) {
-					case guess === null:
-						return 0;
-					case isNaN(guess):
-						guess = Number(prompt(`Please enter a number.`));
-						break;
-					case guess < numToGuess:
-						guess = Number(prompt(`${guess} is too small. Guess a larger number.`));
-						guessCount++;
-						break;
-					case guess > numToGuess:
-						guess = Number(prompt(`${guess} is too large. Guess a smaller number.`));
-						guessCount++;
-						break;
-					case guess === numToGuess:
-						return guessCount;
-				}
-			}
-			return 0;
+			console.log('TTT' + guessCount)
+			return ++guessCount
 		}
+		if (isNaN(guess)) {
+			guess = Number(prompt(`Please enter a number.`));
+			continue;
+		}
+		if (guess < numToGuess) {
+			guess = Number(prompt(`${guess} is too small. Guess a larger number.`));
+		}
+		if (guess > numToGuess) {
+			guess = Number(prompt(`${guess} is too large. Guess a smaller number.`));
+		}
+		guessCount++;
 	}
+	return 0;
 };
