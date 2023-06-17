e3015888bf043c391bf06713972fb80c64a4f4a1
Debarshi Das <debarshi121@gmail.com>
2023-06-17 15:51:20 +0530

    Week-3 Assignment fixed

--- Week-3/Session-09/JavaScript number guessing game/script.js
+++ Week-3/Session-09/JavaScript number guessing game/script.js
@@ -1,24 +1,21 @@
 // Your solution goes here
-const playGuessingGame = (numToGuess, totalGuesses = 10) => {
+// Your solution goes here
+const playGuessingGame = (numToGuess, totalGuesses = 5) => {
 	let guessCount = 0;
-	let guess = Number(prompt(`Enter a number between 1 and 100.`));
+	let guess = prompt(`Enter a number between 1 and 100.`);
 
 	while (guessCount <= totalGuesses) {
 		if (guess === null) return 0;
-		if (guess === 0) return 0;
-		if (guess === numToGuess) {
-			console.log('TTT' + guessCount)
-			return ++guessCount
-		}
+		if (guess == numToGuess) return ++guessCount;
 		if (isNaN(guess)) {
-			guess = Number(prompt(`Please enter a number.`));
+			guess = prompt(`Please enter a number.`);
 			continue;
 		}
 		if (guess < numToGuess) {
-			guess = Number(prompt(`${guess} is too small. Guess a larger number.`));
+			guess = prompt(`${guess} is too small. Guess a larger number.`);
 		}
 		if (guess > numToGuess) {
-			guess = Number(prompt(`${guess} is too large. Guess a smaller number.`));
+			guess = prompt(`${guess} is too large. Guess a smaller number.`);
 		}
 		guessCount++;
 	}
