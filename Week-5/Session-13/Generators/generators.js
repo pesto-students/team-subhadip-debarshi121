53424bfbe950e7cf8ed1adee0a60702fcc636b45
Debarshi Das <debarshi121@gmail.com>
2023-06-18 14:13:00 +0530

    Session-13 Done

---
+++ Week-5/Session-13/Generators/generators.js
@@ -0,0 +1,13 @@
+function* stringSymbols(arr) {
+	for (let str of arr) {
+		yield Symbol(str);
+	}
+}
+
+const arr = ["hello", "world", "test"];
+
+const symbolIterator = stringSymbols(arr);
+
+console.log(symbolIterator.next().value);
+console.log(symbolIterator.next().value);
+console.log(symbolIterator.next().value);
