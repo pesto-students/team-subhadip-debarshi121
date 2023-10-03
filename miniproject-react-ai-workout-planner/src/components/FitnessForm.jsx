f426e7930cf9e35e46c34cad05ae9c6099ec960a
Debarshi Das <debarshi121@gmail.com>
2023-10-03 23:51:44 +0530

    palm

--- miniproject-react-ai-workout-planner/src/components/FitnessForm.jsx
+++ miniproject-react-ai-workout-planner/src/components/FitnessForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import axios from "axios"
+import axios from "axios";
 
 const FitnessForm = () => {
 	const [formData, setFormData] = useState({
@@ -21,16 +21,60 @@ const FitnessForm = () => {
 		});
 	};
 
+	const extractJSON = (output) => {
+		const jsonStartIndex = output.indexOf("[");
+		const jsonEndIndex = output.lastIndexOf("]") + 1;
+		const json = output.substring(jsonStartIndex, jsonEndIndex);
+		// Parse the JSON
+		console.log(JSON.parse(json))
+		return JSON.parse(json);
+	};
+
 	const handleGenerateWorkoutPlan = async (e) => {
 		e.preventDefault();
 
 		const apiUrl = `https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage`;
 
+		const content = `
+			Based on the below user data, generate a excercise plan for a week in JSON.
+			User data:
+			height: 175 cm, weight: 60 kg, age: 28 years, gender: male, fitness level: beginner, fitness goal: muscle building
+			
+			Sample output JSON stringify:
+			
+			[
+				{
+					"day": "Monday",
+					"restDay": false,
+					"exercises: [
+						{
+							"name": "Push Ups",
+							"sets": 3,
+							"repetitions": [10,8,6],
+							"weights": [],
+							"restBetweenSets":[60,30]
+						},
+						{
+							"name": "Pull Ups",
+							"sets": 3,
+							"repetitions": [10,8,6],
+							"weights": [],
+							"restBetweenSets":[60,30]
+						},
+					]
+				}
+			]
+
+			where weights are in kgs if needed for that exercise and restBetweenSets is in seconds.
+			For rest days the restDay should be true & exercises should be an empty array
+
+		`;
+
 		const requestData = {
 			prompt: {
 				context: "",
 				examples: [],
-				messages: [{ content: "Generate a 6 days workout plan for me. My age is 28, my gender is male, fitness level is beginner, height is 5.10 feet & weight is 60kg. Target is weight gain." }],
+				messages: [{ content }],
 			},
 			temperature: 0.25,
 			top_k: 40,
@@ -46,9 +90,11 @@ const FitnessForm = () => {
 			const response = await axios.post(`${apiUrl}?key=${process.env.REACT_APP_GOOGLE_PALM_API_KEY}`, requestData, {
 				headers,
 			});
-			console.log(response)
+			const output = response.data.candidates[0].content;
+
+			return extractJSON(output);
 		} catch (error) {
-			console.log(error)
+			console.log(error);
 		}
 	};
 
