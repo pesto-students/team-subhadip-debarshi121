bd55a49928b612f42e5bff9366c7fa325ad16026
Debarshi <debarshi121@gmail.com>
2023-10-05 12:03:28 +0530

    UI

--- miniproject-react-ai-workout-planner/src/App.js
+++ miniproject-react-ai-workout-planner/src/App.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 function App() {
 	return (
-		<div className="App bg-gray-50">
+		<div className="App bg-gray-50 h-screen">
 			<BrowserRouter>
 				<Header />
 				<Provider store={store}>
