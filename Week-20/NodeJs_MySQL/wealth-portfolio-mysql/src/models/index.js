523f8f535cc409892d170d2baa4de71f75b9baad
Debarshi Das <debarshi121@gmail.com>
2023-12-09 19:35:36 +0530

    week 19

--- Week-20/NodeJs_MySQL/wealth-portfolio-mysql/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require("./userModel");
-const Asset = require("./assetModel");
-const IncomeExpense = require("./incomeExpenseModel");
-
-module.exports = {
-    User,
-    Asset,
-    IncomeExpense
-}
