5c4900d27584fc7ea80087b24a968c636986d36b
Debarshi Das <debarshi121@gmail.com>
2023-12-09 20:58:05 +0530

    week 19 done

--- Week-20/NodeJs_mongoDB/wealth-portfolio-mongodb/src/db/index.js
+++
@@ -1,4 +0,0 @@
-const config = require("../config");
-const DB = `mongodb+srv://${config.DB.USERNAME}:${config.DB.PASSWORD}@${config.DB.CLUSTER}.pxx6wyi.mongodb.net/${config.DB.NAME}?retryWrites=true&w=majority`;
-
-module.exports = DB;
